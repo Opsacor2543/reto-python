a0320074a3473a1a3aeeef4f673f097ac95ae4b8
Opsacor2543 <159188027+Opsacor2543@users.noreply.github.com>
2024-02-06 13:43:51 -0600

    Add files via upload

---
+++ reto1.py
@@ -0,0 +1,13 @@
+#variables, tipos de datos
+
+def reto1():
+    name = str(input("ingresa tu nombre(s)-> "));
+    apellidos = str(input("ingresa tus apellidos-> "));
+    numero = int(input("ingresa tu numero de telefono-> "));
+    correo= str(input("ingresa tu correo->"));
+
+    print("Hola ", name,apellidos," en breve recibiras un correo a ",correo);
+
+reto1();
+
+     
